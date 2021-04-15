696fa4f786c8498176ee85c34f62a6d69fe52bfc
Jean Luca Colombo <31295051+JeanLColombo@users.noreply.github.com>
2021-04-15 16:17:39 -0300

    Implemented Position Class.

---
+++ Source/Core/Abstractions/Enumerations.cs
@@ -0,0 +1,26 @@
+namespace Core.Abstractions
+{
+    public enum Files
+    {
+        a = 0,
+        b = 1,
+        c = 2,
+        d = 3,
+        e = 4,
+        f = 5,
+        g = 6,
+        h = 7
+    }
+
+    public enum Ranks
+    {
+        one = 0,
+        two = 1,
+        three = 2,
+        four = 3,
+        five = 4,
+        six = 5,
+        seven = 6,
+        eigth = 7
+    }
+}
---
+++ UML Diagram/README.md
@@ -0,0 +1,3 @@
+# UML Diagram
+
+We will be completely refatoring [Mate.NET](https://github.com/JeanLColombo/Mate.Net). For that purpose, each application will be redone from scrath, following an UML methodology.
