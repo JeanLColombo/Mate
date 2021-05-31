c81a8dea251364406eb5559d6492575661078fd6
Jean Luca Colombo <31295051+JeanLColombo@users.noreply.github.com>
2021-05-31 18:22:04 -0300

    King implemented and fully tested.

---
+++ Source/Core/Elements/Pieces/King.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Core.Abstractions;
+
+
+namespace Core.Elements.Pieces
+{
+    /// <summary>
+    /// Implements the <see cref="King"/> piece.
+    /// </summary>
+    public class King : Royalty
+    {
+        /// <summary>
+        /// Creates a <see cref="King"/> piece of given <paramref name="color"/>.
+        /// </summary>
+        /// <param name="color">True for white. Black otherwise.</param>
+        /// <returns></returns>
+        public King(bool color) : base(color) {}
+
+        /// <summary>
+        /// Returns all moves available for a <see cref="King"/> based on a board <paramref name="position"/>. 
+        /// </summary>
+        /// <param name="position">A given <see cref="Board.Position"/>.</param>
+        /// <returns></returns>
+        public override IReadOnlyCollection<Move> AvailableMoves(IReadOnlyDictionary<Square,IPiece> position) =>
+            RoyalAttack(position, 1);
+    }
+}
--- Test/Core/Elements/Pieces/TestKing.cs
+++ Test/Core/Elements/Pieces/TestKing.cs
@@ -9,33 +9,42 @@ using Tests.Core.Mocks;
 
 namespace Tests.Core.Elements.Pieces
 {
-    public class TestQueen
+    public class TestKing
     {
         [Fact]
-        public void TestStarOnTheBoard()
+        public void TestKingOfTheHill()
         {
             var board = new Board();
 
-            board.AddPiece<Queen>(SquareEFive, true);
+            board.AddPiece<King>(SquareEFive, true);
             
             board.AddPiece<MockedPiece>(new Square(Files.e, Ranks.six), true);
             board.AddPiece<MockedPiece>(new Square(Files.f, Ranks.six), false);
             board.AddPiece<MockedPiece>(new Square(Files.g, Ranks.five), true);
-            board.AddPiece<MockedPiece>(new Square(Files.h, Ranks.two), false);
+            board.AddPiece<MockedPiece>(new Square(Files.f, Ranks.four), false);
             board.AddPiece<MockedPiece>(new Square(Files.e, Ranks.two), true);
             board.AddPiece<MockedPiece>(new Square(Files.a, Ranks.one), false);
-            board.AddPiece<MockedPiece>(new Square(Files.c, Ranks.five), true);
+            board.AddPiece<MockedPiece>(new Square(Files.d, Ranks.five), true);
             board.AddPiece<MockedPiece>(new Square(Files.b, Ranks.eigth), false);
 
             var moves = board.Position[SquareEFive].AvailableMoves(board.Position);
 
-            Assert.Equal(15, moves.Count);
-            Assert.Equal(11, moves.Where(m => m.Type is MoveType.Normal).ToList().Count());
-            Assert.Equal(4, moves.Where(m => m.Type is MoveType.Capture).ToList().Count());
+            Assert.Equal(6, moves.Count);
+            Assert.Equal(4, moves.Where(m => m.Type is MoveType.Normal).ToList().Count());
+            Assert.Equal(2, moves.Where(m => m.Type is MoveType.Capture).ToList().Count());
             Assert.All(moves.Select(m => m.FromSquare), (s) => Assert.Equal(SquareEFive, s));
 
-            Assert.All(board.Position.Where(p => p.Value.Color is false).Select(p => p.Key), 
-                (s) => Assert.Contains(s, moves.Where(m => m.Type is MoveType.Capture).Select(m => m.ToSquare).ToList()));
+            Assert.All(new Square[] 
+                {
+                    new Square(Files.f, Ranks.six), 
+                    new Square(Files.f, Ranks.four)
+                }, (s) => 
+                Assert.Contains(
+                    s, 
+                    moves
+                        .Where(m => m.Type is MoveType.Capture)
+                        .Select(m => m.ToSquare)
+                        .ToList()));
         }
 
         private Square SquareEFive => new Square(Files.e, Ranks.five);
